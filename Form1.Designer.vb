651dd061272aa83a24625cc3d8e4d54bfe81b687
Jack Morter <75439453+JackMorter@users.noreply.github.com>
2021-08-11 12:07:57 +0200

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frm_launcher
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_launcher))
+        Me.pb_play = New System.Windows.Forms.PictureBox()
+        Me.pb_cache = New System.Windows.Forms.PictureBox()
+        Me.pb_website = New System.Windows.Forms.PictureBox()
+        Me.b_close = New System.Windows.Forms.Button()
+        Me.b_minimize = New System.Windows.Forms.Button()
+        Me.progress_load = New System.Windows.Forms.ProgressBar()
+        Me.lbl_update = New System.Windows.Forms.Label()
+        CType(Me.pb_play, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pb_cache, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pb_website, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pb_play
+        '
+        Me.pb_play.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pb_play.BackColor = System.Drawing.Color.Transparent
+        Me.pb_play.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.pb_play.Image = Global.Winterspring_Launcher.My.Resources.Resources.play_static
+        Me.pb_play.Location = New System.Drawing.Point(328, 320)
+        Me.pb_play.Name = "pb_play"
+        Me.pb_play.Size = New System.Drawing.Size(181, 118)
+        Me.pb_play.TabIndex = 0
+        Me.pb_play.TabStop = False
+        '
+        'pb_cache
+        '
+        Me.pb_cache.BackColor = System.Drawing.Color.Transparent
+        Me.pb_cache.Image = Global.Winterspring_Launcher.My.Resources.Resources.cache_static
+        Me.pb_cache.Location = New System.Drawing.Point(173, 376)
+        Me.pb_cache.Name = "pb_cache"
+        Me.pb_cache.Size = New System.Drawing.Size(161, 62)
+        Me.pb_cache.TabIndex = 1
+        Me.pb_cache.TabStop = False
+        '
+        'pb_website
+        '
+        Me.pb_website.BackColor = System.Drawing.Color.Transparent
+        Me.pb_website.Image = Global.Winterspring_Launcher.My.Resources.Resources.web_static
+        Me.pb_website.Location = New System.Drawing.Point(12, 376)
+        Me.pb_website.Name = "pb_website"
+        Me.pb_website.Size = New System.Drawing.Size(161, 62)
+        Me.pb_website.TabIndex = 1
+        Me.pb_website.TabStop = False
+        '
+        'b_close
+        '
+        Me.b_close.BackColor = System.Drawing.Color.Transparent
+        Me.b_close.Location = New System.Drawing.Point(505, 4)
+        Me.b_close.Name = "b_close"
+        Me.b_close.Size = New System.Drawing.Size(20, 20)
+        Me.b_close.TabIndex = 2
+        Me.b_close.TabStop = False
+        Me.b_close.Text = "x"
+        Me.b_close.UseVisualStyleBackColor = False
+        '
+        'b_minimize
+        '
+        Me.b_minimize.BackColor = System.Drawing.Color.Transparent
+        Me.b_minimize.Location = New System.Drawing.Point(484, 4)
+        Me.b_minimize.Name = "b_minimize"
+        Me.b_minimize.Size = New System.Drawing.Size(20, 20)
+        Me.b_minimize.TabIndex = 2
+        Me.b_minimize.TabStop = False
+        Me.b_minimize.Text = "-"
+        Me.b_minimize.UseVisualStyleBackColor = False
+        '
+        'progress_load
+        '
+        Me.progress_load.Location = New System.Drawing.Point(111, 353)
+        Me.progress_load.Name = "progress_load"
+        Me.progress_load.Size = New System.Drawing.Size(211, 23)
+        Me.progress_load.Step = 5
+        Me.progress_load.TabIndex = 3
+        Me.progress_load.Visible = False
+        '
+        'lbl_update
+        '
+        Me.lbl_update.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lbl_update.AutoSize = True
+        Me.lbl_update.BackColor = System.Drawing.Color.Transparent
+        Me.lbl_update.ForeColor = System.Drawing.Color.White
+        Me.lbl_update.Location = New System.Drawing.Point(27, 357)
+        Me.lbl_update.Name = "lbl_update"
+        Me.lbl_update.Size = New System.Drawing.Size(0, 13)
+        Me.lbl_update.TabIndex = 4
+        '
+        'frm_launcher
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Winterspring_Launcher.My.Resources.Resources.background
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(528, 450)
+        Me.Controls.Add(Me.lbl_update)
+        Me.Controls.Add(Me.b_minimize)
+        Me.Controls.Add(Me.b_close)
+        Me.Controls.Add(Me.pb_website)
+        Me.Controls.Add(Me.pb_cache)
+        Me.Controls.Add(Me.pb_play)
+        Me.Controls.Add(Me.progress_load)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frm_launcher"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Winterspring Launcher"
+        CType(Me.pb_play, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pb_cache, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pb_website, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents pb_play As PictureBox
+    Friend WithEvents pb_cache As PictureBox
+    Friend WithEvents pb_website As PictureBox
+    Friend WithEvents b_close As Button
+    Friend WithEvents b_minimize As Button
+    Friend WithEvents progress_load As ProgressBar
+    Friend WithEvents lbl_update As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,145 @@
+﻿Imports System.IO
+Imports System.Net
+
+Public Class frm_launcher
+    'Declare the variables
+    Dim drag As Boolean
+    Dim mousex As Integer
+    Dim mousey As Integer
+
+    Private WithEvents WebC As New WebClient
+
+    'Dragable Form
+    Private Sub Form1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseDown
+        drag = True 'Sets the variable drag to true.
+        mousex = Windows.Forms.Cursor.Position.X - Me.Left 'Sets variable mousex
+        mousey = Windows.Forms.Cursor.Position.Y - Me.Top 'Sets variable mousey
+    End Sub
+    Private Sub Form1_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseUp
+        drag = False 'Sets drag to false, so the form does not move according to the code in MouseMove
+    End Sub
+    Private Sub Form1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseMove
+        'If drag is set to true then move the form accordingly.
+        If drag Then
+            Me.Top = Windows.Forms.Cursor.Position.Y - mousey
+            Me.Left = Windows.Forms.Cursor.Position.X - mousex
+        End If
+    End Sub
+
+    'ProgressBar
+    Private Sub client_ProgressChanged(ByVal sender As Object, ByVal e As DownloadProgressChangedEventArgs)
+        Dim bytesIn As Double = Double.Parse(e.BytesReceived.ToString())
+        Dim totalBytes As Double = Double.Parse(e.TotalBytesToReceive.ToString())
+        Dim percentage As Double = bytesIn / totalBytes * 100
+
+        progress_load.Value = Int32.Parse(Math.Truncate(percentage).ToString())
+    End Sub
+
+    'Download Function
+    Private Sub download_patch()
+        Dim client As WebClient = New WebClient
+        Dim local_path As String = My.Computer.FileSystem.CurrentDirectory & "\Data\Patch-Z.MPQ"
+
+        AddHandler client.DownloadProgressChanged, AddressOf client_ProgressChanged
+        AddHandler client.DownloadFileCompleted, AddressOf client_DownloadCompleted
+
+        client.DownloadFileAsync(New Uri("https://download.winterspring.online/WOW/Patch-Z.MPQ"), local_path)
+
+        progress_load.Visible = True
+
+        lbl_update.Visible = True
+        lbl_update.Text = "Downloading..."
+
+        pb_play.Enabled = False
+        pb_cache.Enabled = False
+    End Sub
+
+    'Download Complete
+    Private Sub client_DownloadCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.AsyncCompletedEventArgs)
+
+        pb_play.Enabled = True
+        pb_cache.Enabled = True
+
+        lbl_update.Text = "Up to date!"
+    End Sub
+
+    '################################################ BUTTONS ##################################################
+
+    'Play Button
+    Private Sub pb_play_Click(sender As Object, e As EventArgs) Handles pb_play.Click
+        If File.Exists(My.Computer.FileSystem.CurrentDirectory & "/Data/Patch-Z.MPQ") Then
+
+            If File.Exists(My.Computer.FileSystem.CurrentDirectory & "/Wow.exe") Then
+                Shell(My.Computer.FileSystem.CurrentDirectory & "/Wow.exe")
+                Me.Close()
+            Else
+                MsgBox("Could not start Wow.exe", MsgBoxStyle.Critical, "Application Not Found")
+            End If
+
+        Else
+            'Download Patch-Z from WebSite
+            download_patch()
+
+        End If
+    End Sub
+
+    'Cache Button
+    Private Sub pb_cache_Click(sender As Object, e As EventArgs) Handles pb_cache.Click
+        pb_cache.Image = My.Resources.cache_pressed
+
+        If Directory.Exists(My.Computer.FileSystem.CurrentDirectory & "/Cache") Then
+
+            Dim Cache As String = (My.Computer.FileSystem.CurrentDirectory & "/Cache")
+
+            Select Case MsgBox("Do you want to delete the Cache folder?", MsgBoxStyle.Critical + MsgBoxStyle.YesNo, "WARNING")
+                Case MsgBoxResult.Yes
+                    Directory.Delete(Cache, True)
+
+                Case MsgBoxResult.No
+
+            End Select
+        End If
+    End Sub
+
+    'Website Buton
+    Private Sub pb_website_Click(sender As Object, e As EventArgs) Handles pb_website.Click
+        pb_website.Image = My.Resources.web_pressed
+        Process.Start("https://winterspring.online")
+    End Sub
+
+    'Minimize
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles b_minimize.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    'Close
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles b_close.Click
+        Me.Close()
+    End Sub
+
+    'Button Hover
+    Private Sub pb_play_MouseHover(sender As Object, e As EventArgs) Handles pb_play.MouseEnter
+        pb_play.Image = My.Resources.play_pressed
+    End Sub
+
+    Private Sub pb_play_MouseLeave(sender As Object, e As EventArgs) Handles pb_play.MouseLeave
+        pb_play.Image = My.Resources.play_static
+    End Sub
+
+    Private Sub pb_cache_MouseEnter(sender As Object, e As EventArgs) Handles pb_cache.MouseEnter
+        pb_cache.Image = My.Resources.cache_pressed
+    End Sub
+
+    Private Sub pb_cache_MouseLeave(sender As Object, e As EventArgs) Handles pb_cache.MouseLeave
+        pb_cache.Image = My.Resources.cache_static
+    End Sub
+
+    Private Sub pb_website_MouseEnter(sender As Object, e As EventArgs) Handles pb_website.MouseEnter
+        pb_website.Image = My.Resources.web_pressed
+    End Sub
+
+    Private Sub pb_website_MouseLeave(sender As Object, e As EventArgs) Handles pb_website.MouseLeave
+        pb_website.Image = My.Resources.web_static
+    End Sub
+
+End Class
